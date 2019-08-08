bf8a487a88bfdcd3b30cdc17d1acab704194997f
mohamed mostapha <mohamed.mostapha3699@gmail.com>
2019-08-08 08:39:15 -0700

    another update

--- main.js
+++ main.js
@@ -13,122 +13,122 @@ function toggleSidebar() {
   
   
   
-  var myVar, myVar2, myVar3;
+  // var myVar, myVar2, myVar3;
   
-  var currentBackground2019 = 0,
-      backgrounds2019 = [];
+  // var currentBackground2019 = 0,
+  //     backgrounds2019 = [];
   
-      backgrounds2019[0] = '/images/main01.jpg';
-      backgrounds2019[1] = '/images/main02.jpg';
-      backgrounds2019[2] = '/images/main03.jpg';
+  //     backgrounds2019[0] = '/images/main01.jpg';
+  //     backgrounds2019[1] = '/images/main02.jpg';
+  //     backgrounds2019[2] = '/images/main03.jpg';
   
-  function changeBackground2019() {
-    currentBackground2019++;
-    if(currentBackground2019 > 2) currentBackground2019 = 0;
+  // function changeBackground2019() {
+  //   currentBackground2019++;
+  //   if(currentBackground2019 > 2) currentBackground2019 = 0;
   
   
-    $('.ratio').fadeOut(100, function() {
-      $('.ratio').css({
-        'background-image': "url('" + backgrounds2019[currentBackground2019] + "')", 'background-repeat': 'no-repeat', 'background-size': 'cover'
-      });
+  //   $('.ratio').fadeOut(100, function() {
+  //     $('.ratio').css({
+  //       'background-image': "url('" + backgrounds2019[currentBackground2019] + "')", 'background-repeat': 'no-repeat', 'background-size': 'cover'
+  //     });
   
-      $('.ratio').fadeIn(100);
+  //     $('.ratio').fadeIn(100);
   
-    });
-  
-    
-  
-    myVar = setTimeout(changeBackground2019, 3000);
+  //   });
   
     
-  }
   
-  
-  $('.slick-item-1').on("click", changeBackground2019);
-  
-  $('.slick-item-5').on("click", changeBackground2018);
-  
-  $('.slick-item-9').on("click", changeBackground2017);
-  
-  
-  
-  $('.slick-item-1').on("click", function() {
-    clearTimeout(myVar2);
-    clearTimeout(myVar3);
-  })
-  
-  
-  $('.slick-item-5').on("click", function() {
-    clearTimeout(myVar);
-    clearTimeout(myVar3);
-  });
-  
-  
-  $('.slick-item-9').on("click", function() {
-    clearTimeout(myVar);
-    clearTimeout(myVar2);
-  });
-  
-  
-  
-  
-  var currentBackground2018 = 0,
-      backgrounds2018 = [];
-  
-      backgrounds2018[0] = '/images/main04.jpg';
-      backgrounds2018[1] = '/images/main05.jpg';
-      backgrounds2018[2] = '/images/main06.jpg';
-  
-  function changeBackground2018() {
+  //   myVar = setTimeout(changeBackground2019, 3000);
   
     
-    currentBackground2018++;
-    if(currentBackground2018 > 2) currentBackground2018 = 0;
+  // }
+  
+  
+  // $('.slick-item-1').on("click", changeBackground2019);
+  
+  // $('.slick-item-5').on("click", changeBackground2018);
+  
+  // $('.slick-item-9').on("click", changeBackground2017);
+  
+  
+  
+  // $('.slick-item-1').on("click", function() {
+  //   clearTimeout(myVar2);
+  //   clearTimeout(myVar3);
+  // })
+  
+  
+  // $('.slick-item-5').on("click", function() {
+  //   clearTimeout(myVar);
+  //   clearTimeout(myVar3);
+  // });
+  
+  
+  // $('.slick-item-9').on("click", function() {
+  //   clearTimeout(myVar);
+  //   clearTimeout(myVar2);
+  // });
+  
+  
+  
+  
+  // var currentBackground2018 = 0,
+  //     backgrounds2018 = [];
+  
+  //     backgrounds2018[0] = '/images/main04.jpg';
+  //     backgrounds2018[1] = '/images/main05.jpg';
+  //     backgrounds2018[2] = '/images/main06.jpg';
+  
+  // function changeBackground2018() {
+  
+    
+  //   currentBackground2018++;
+  //   if(currentBackground2018 > 2) currentBackground2018 = 0;
     
   
-    $('.ratio').fadeOut(100, function() {
-      $('.ratio').css({
-        'background-image': "url('" + backgrounds2018[currentBackground2018] + "')", 'background-repeat': 'no-repeat', 'background-size': 'cover'
-      });
+  //   $('.ratio').fadeOut(100, function() {
+  //     $('.ratio').css({
+  //       'background-image': "url('" + backgrounds2018[currentBackground2018] + "')", 'background-repeat': 'no-repeat', 'background-size': 'cover'
+  //     });
   
-      $('.ratio').fadeIn(100);
+  //     $('.ratio').fadeIn(100);
   
-    });
+  //   });
   
-      myVar2 = setTimeout(changeBackground2018, 3000)
-  
-  
-  }
+  //     myVar2 = setTimeout(changeBackground2018, 3000)
   
   
+  // }
   
-      var currentBackground2017 = 0,
-      backgrounds2017 = [];
   
-      backgrounds2017[0] = '/images/main07.jpg';
-      backgrounds2017[1] = '/images/main08.jpg';
-      backgrounds2017[2] = '/images/main09.jpg';
   
-      function changeBackground2017() {
+  //     var currentBackground2017 = 0,
+  //     backgrounds2017 = [];
+  
+  //     backgrounds2017[0] = '/images/main07.jpg';
+  //     backgrounds2017[1] = '/images/main08.jpg';
+  //     backgrounds2017[2] = '/images/main09.jpg';
+  
+  //     function changeBackground2017() {
   
     
-      currentBackground2017++;
-      if(currentBackground2017 > 2) currentBackground2017 = 0;
+  //     currentBackground2017++;
+  //     if(currentBackground2017 > 2) currentBackground2017 = 0;
     
   
-      $('.ratio').fadeOut(100, function() {
-        $('.ratio').css({
-          'background-image': "url('" + backgrounds2017[currentBackground2017] + "')", 'background-repeat': 'no-repeat', 'background-size': 'cover'
-       });
+  //     $('.ratio').fadeOut(100, function() {
+  //       $('.ratio').css({
+  //         'background-image': "url('" + backgrounds2017[currentBackground2017] + "')", 'background-repeat': 'no-repeat', 'background-size': 'cover'
+  //      });
   
-      $('.ratio').fadeIn(100);
+  //     $('.ratio').fadeIn(100);
   
-      });
+  //     });
   
-      myVar3 = setTimeout(changeBackground2017, 3000)
+  //     myVar3 = setTimeout(changeBackground2017, 3000)
   
   
-  }
+  // }
   
   
   
@@ -200,30 +200,6 @@ function toggleSidebar() {
   
 
     /*Project slider */
-    var slideIndex = 1;
-showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
-}
     /*end project slider*/
