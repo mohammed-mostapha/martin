d2a0923860ac2ddeb024a3a2d0afab3fddeefb6e
mohamed mostapha <mohamed.mostapha3699@gmail.com>
2019-08-15 20:40:49 -0700

    added project description

--- main.js
+++ main.js
@@ -17,25 +17,47 @@ function toggleSidebar() {
   
   var currentBackground20191 = 0,
       backgrounds20191 = [];
+
   
       backgrounds20191[0] = '/images/main01.jpg';
       backgrounds20191[1] = '/images/main02.jpg';
       backgrounds20191[2] = '/images/main03.jpg';
+
+
+
+
+      var currentProjectDescription20191 = 0,
+      projectDescription20191 = [];
+
+      projectDescription20191[0] = "P2019-1-a / about.....";
+      projectDescription20191[1] = "P2019-1-b / about.....";
+      projectDescription20191[2] = "P2019-1-c / about.....";
+
+
   
   function changeBackground20191() {
     currentBackground20191++;
     if(currentBackground20191 > 2) currentBackground20191 = 0;
+
+    currentProjectDescription20191++;
+    if(currentProjectDescription20191 > 2) currentProjectDescription20191 = 0;
   
+    
+ 
   
     $('#ratio2019-1').fadeOut(100, function() {
       $('#ratio2019-1').css({
         'background': "url('" + backgrounds20191[currentBackground20191] + "')", 'background-repeat': 'no-repeat', 'background-size': 'cover'
       });
-  
+
+      
       $('#ratio2019-1').fadeIn(100);
   
     });
-  
+    
+    
+    document.querySelector('#project-description20191').innerHTML = "" + projectDescription20191[currentProjectDescription20191] + "";
+   
 
   
     myVar20191 = setTimeout(changeBackground20191, 3000);
@@ -126,26 +148,12 @@ function toggleSidebar() {
 
 
 
-
-
-
-
-
-
-
-  
-
-
   $('.slick-item-2').on("click", changeBackground20191);
   $('.slick-item-3').on("click", changeBackground20192);
   $('.slick-item-4').on("click", changeBackground20193);
   
   
 
-
- 
-  
-  
   
   
   var currentBackground20192 = 0,
@@ -154,12 +162,22 @@ function toggleSidebar() {
       backgrounds20192[0] = '/images/main04.jpg';
       backgrounds20192[1] = '/images/main05.jpg';
       backgrounds20192[2] = '/images/main06.jpg';
+
+      var currentProjectDescription20192 = 0,
+      projectDescription20192 = [];
+
+      projectDescription20192[0] = "P2019-2-a / about.....";
+      projectDescription20192[1] = "P2019-2-b / about.....";
+      projectDescription20192[2] = "P2019-2-c / about.....";
   
   function changeBackground20192() {
   
     
     currentBackground20192++;
     if(currentBackground20192 > 2) currentBackground20192 = 0;
+
+    currentProjectDescription20192++;
+    if(currentProjectDescription20192 > 2) currentProjectDescription20192 = 0;
     
   
     $('#ratio2019-2').fadeOut(100, function() {
@@ -171,6 +189,8 @@ function toggleSidebar() {
   
     });
   
+    document.querySelector('#project-description20192').innerHTML = "" + projectDescription20192[currentProjectDescription20192] + "";
+
       myVar20192 = setTimeout(changeBackground20192, 3000)
   
   
@@ -181,15 +201,26 @@ function toggleSidebar() {
       var currentBackground20193 = 0,
       backgrounds20193 = [];
   
-      backgrounds20193[0] = '/images/main07.png';
+      backgrounds20193[0] = '/images/main07.jpg';
       backgrounds20193[1] = '/images/main08.jpg';
       backgrounds20193[2] = '/images/main09.jpg';
+
+      var currentProjectDescription20193 = 0,
+      projectDescription20193 = [];
+
+      projectDescription20193[0] = "P2019-3-a / about.....";
+      projectDescription20193[1] = "P2019-3-b / about.....";
+      projectDescription20193[2] = "P2019-3-c / about.....";
+
   
       function changeBackground20193() {
   
     
       currentBackground20193++;
       if(currentBackground20193 > 2) currentBackground20193 = 0;
+
+      currentProjectDescription20193++;
+      if(currentProjectDescription20193 > 2) currentProjectDescription20193 = 0;
     
   
       $('#ratio2019-3').fadeOut(100, function() {
@@ -201,6 +232,8 @@ function toggleSidebar() {
   
       });
   
+      document.querySelector('#project-description20193').innerHTML = "" + projectDescription20193[currentProjectDescription20193] + "";
+
       myVar20193 = setTimeout(changeBackground20193, 3000)
   
   
@@ -213,6 +246,7 @@ function toggleSidebar() {
     /*Project change 2019-1*/
 
     $('.p2019-1-a').click(function() {
+      document.querySelector('#project-description20191').innerHTML="P2019-1-a / about.....";
       $('#ratio2019-1').css({
         'background-image': "url('images/main01.jpg')"
       });
@@ -222,6 +256,7 @@ function toggleSidebar() {
     });
 
     $('.p2019-1-b').click(function() {
+      document.querySelector('#project-description20191').innerHTML="P2019-1-b / about.....";
       $('#ratio2019-1').css({
         'background-image': "url('images/main11.jpg')"
       });
@@ -232,6 +267,7 @@ function toggleSidebar() {
     });
 
     $('.p2019-1-c').click(function() {
+      document.querySelector('#project-description20191').innerHTML="P2019-1-c / about.....";
       $('#ratio2019-1').css({
         'background-image': "url('images/main12.jpg')"
       });
@@ -246,6 +282,7 @@ function toggleSidebar() {
     /*Project change 2019-2*/
 
     $('.p2019-2-a').click(function() {
+      document.querySelector('#project-description20192').innerHTML="P2019-2-a / about.....";
       $('#ratio2019-2').css({
         'background-image': "url('images/main04.jpg')"
       });
@@ -255,6 +292,7 @@ function toggleSidebar() {
     });
 
     $('.p2019-2-b').click(function() {
+      document.querySelector('#project-description20192').innerHTML="P2019-2-b / about.....";
       $('#ratio2019-2').css({
         'background-image': "url('images/main05.jpg')"
       });
@@ -265,6 +303,7 @@ function toggleSidebar() {
     });
 
     $('.p2019-2-c').click(function() {
+      document.querySelector('#project-description20192').innerHTML="P2019-2-c / about.....";
       $('#ratio2019-2').css({
         'background-image': "url('images/main06.jpg')"
       });
@@ -280,6 +319,7 @@ function toggleSidebar() {
     /*Project change 2019-3*/
 
     $('.p2019-3-a').click(function() {
+      document.querySelector('#project-description20193').innerHTML="P2019-3-a / about.....";
       $('#ratio2019-3').css({
         'background-image': "url('images/main07.jpg')"
       });
@@ -289,6 +329,7 @@ function toggleSidebar() {
     });
 
     $('.p2019-3-b').click(function() {
+      document.querySelector('#project-description20193').innerHTML="P2019-3-b / about.....";
       $('#ratio2019-3').css({
         'background-image': "url('images/main08.jpg')"
       });
@@ -299,6 +340,7 @@ function toggleSidebar() {
     });
 
     $('.p2019-3-c').click(function() {
+      document.querySelector('#project-description20193').innerHTML="P2019-3-c / about.....";
       $('#ratio2019-3').css({
         'background-image': "url('images/main09.jpg')"
       });
@@ -310,7 +352,13 @@ function toggleSidebar() {
 
     /* end Project change 2019-3*/
 
+    
+  
+   /* project description paragraph */
 
+  
+
+   /* end project description paragraph */
 
 
   
